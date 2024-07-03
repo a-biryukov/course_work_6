1037d6569087408df7d8c709bbe47057bbb9b354
Alexander <a.biryukov164@gmail.com>
2024-07-03 20:14:44 +0400

    Реализовал CRUD для сообщений

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,16 +1,25 @@
 from django.urls import path
 from mailings.apps import MailingsConfig
 from mailings.views import MailingCreateView, MailingListView, CarouselTemplateView, MessageCreateView, \
-    ClientCreateView, MailingDetailView, MailingDeleteView
+    ClientCreateView, MailingDetailView, MailingDeleteView, MailingUpdateView, MessageListView, MessageDetailView, \
+    MessageUpdateView, MessageDeleteView
 
 app_name = MailingsConfig.name
 
 urlpatterns = [
     path('', CarouselTemplateView.as_view(), name='carousel'),
-    path('mailings-create/', MailingCreateView.as_view(), name='mailing_create'),
+
     path('mailings/', MailingListView.as_view(), name='mailing_list'),
+    path('mailing-create/', MailingCreateView.as_view(), name='mailing_create'),
     path('mailing/<int:pk>/', MailingDetailView.as_view(), name='mailing_detail'),
+    path('mailing-update/<int:pk>/', MailingUpdateView.as_view(), name='mailing_update'),
     path('mailing-delete/<int:pk>/', MailingDeleteView.as_view(), name='mailing_delete'),
+
+    path('messages/', MessageListView.as_view(), name='message_list'),
     path('message-create/', MessageCreateView.as_view(), name='message_create'),
+    path('message/<int:pk>/', MessageDetailView.as_view(), name='message_detail'),
+    path('message-update/<int:pk>/', MessageUpdateView.as_view(), name='message_update'),
+    path('message-delete/<int:pk>/', MessageDeleteView.as_view(), name='message_delete'),
+
     path('client-create/', ClientCreateView.as_view(), name='client_create')
     ]
--- mailings/views.py
+++ mailings/views.py
@@ -1,5 +1,5 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView, ListView, DetailView, DeleteView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, TemplateView, ListView, DetailView, DeleteView, UpdateView
 
 from mailings.forms import MailingForm, MessageForm, ClientForm
 from mailings.models import Mailing, Message, Client
@@ -19,15 +19,44 @@ class MailingDetailView(DetailView):
     model = Mailing
 
 
+class MailingUpdateView(UpdateView):
+    model = Mailing
+    form_class = MailingForm
+
+    def get_success_url(self):
+        return reverse('mailings:mailing_detail', args=[self.kwargs.get('pk')])
+
+
 class MailingDeleteView(DeleteView):
     model = Mailing
     success_url = reverse_lazy('mailing:mailing_list')
+
+
+class MessageListView(ListView):
+    model = Message
 
 
 class MessageCreateView(CreateView):
     model = Message
     form_class = MessageForm
     success_url = reverse_lazy('mailings:client_create')
+
+
+class MessageDetailView(DetailView):
+    model = Message
+
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    form_class = MessageForm
+
+    def get_success_url(self):
+        return reverse('mailings:message_detail', args=[self.kwargs.get('pk')])
+
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    success_url = reverse_lazy('mailings:message_list')
 
 
 class ClientCreateView(CreateView):
