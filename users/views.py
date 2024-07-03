e0a136e42d5bd14045ecf1137aef443b085cc96e
Alexander <a.biryukov164@gmail.com>
2024-07-03 11:12:06 +0400

    реализовал вход и выход из аккаунта

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
 
-# Create your views here.
+from users.forms import UserRegisterForm, RecoveryForm
+from users.models import User
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy("users:login")
+
+
+class RecoveryTemplateView(TemplateView):
+    template_name = 'users/password_recovery.html'
+    form_class = RecoveryForm
+    success_url = reverse_lazy("users:login")
