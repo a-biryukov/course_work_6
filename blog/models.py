691137754ca4e99819acc08ee793af9f28192cca
Alexander <a.biryukov164@gmail.com>
2024-07-06 17:50:56 +0400

    добавил блог

---
+++ blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from config.settings import NULLABLE
+from users.models import User
+
+
+class Blog(models.Model):
+    tittle = models.CharField(max_length=100, verbose_name='Заголовок', help_text='Введите заголовок')
+    text = models.TextField(verbose_name='Текст', help_text='Введите текст')
+    image = models.ImageField(upload_to='blog/images', **NULLABLE, verbose_name='Изображение', help_text='Загрузите изображение')
+    created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True)
+    author = models.ForeignKey(User, verbose_name="Автор", help_text="Введите автора", **NULLABLE, on_delete=models.SET_NULL)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    views_count = models.IntegerField(default=0, verbose_name=' Просмотры')
+    slug = models.CharField(max_length=100, verbose_name='Slug', **NULLABLE)
+
+    def __str__(self):
+        return self.tittle
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
+        permissions = [
+            ('can_edit_publication', 'Can edit publication'),
+            ('can_edit_tittle', 'Can edit tittle'),
+            ('can_edit_text', 'Can edit text'),
+            ('can_edit_image', 'Can edit image')
+        ]
+
--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     'mailings',
     'users',
+    'blog',
     'django_apscheduler'
 ]
 
