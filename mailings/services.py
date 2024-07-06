1a73545c217ab8f20baedaf198e5333c3b1b11d1
Alexander <a.biryukov164@gmail.com>
2024-07-06 11:28:49 +0400

    выделил из основной функции несколько функций

--- mailings/services.py
+++ mailings/services.py
@@ -44,19 +44,20 @@ def start():
 
 
 def send_mailing():
-    """Проверяет дату, время, отправляет сообщение, если нужно и сохраняет логи"""
+    """
+    Получает подходящие по дате рассылки, устанавливает статус 'Запущена' или 'Завершена',
+    отправляет сообщения, устанавливает дату следующей отправки и сохраняет логи
+    """
     current_date = datetime.now().date()
     current_time = datetime.now().time()
+
     mailings = Mailing.objects.filter(
         start_mailing__lte=current_date,
         end_mailing__gte=current_date
     )
 
     for mailing in mailings:
-        if mailing.status != Mailing.STARTED and mailing.end_mailing >= current_date >= mailing.start_mailing :
-            mailing.status = Mailing.STARTED
-            mailing.save()
-
+        make_status(mailing)
         if mailing.next_sending == current_date and mailing.time_sending <= current_time:
             try:
                 server_response = email_send(mailing, fail_silently=False)
@@ -64,19 +65,37 @@ def send_mailing():
                 server_response = e
                 status = False
             else:
-
-                if mailing.end_mailing == current_date and mailing.time_sending <= current_time:
-                    mailing.status = Mailing.COMPLETED
-                    mailing.save()
-
-                if mailing.periodicity == mailing.DAILY:
-                    mailing.next_sending += timedelta(days=1)
-                elif mailing.periodicity == mailing.WEEKLY:
-                    mailing.next_sending += timedelta(weeks=1)
-                elif mailing.periodicity == mailing.MONTHLY:
-                    mailing.next_sending += timedelta(days=30)
-                mailing.save()
+                change_date_next_sending(mailing)
+                print(mailing.next_sending)
                 status = True
-
             finally:
                 Log.objects.create(status=status, server_response=server_response, mailing=mailing)
+
+
+def make_status(obj) -> None:
+    """Проверяет дату и устанавливает соответствующий статус 'Запущена' или 'Завершена'"""
+    current_date = datetime.now().date()
+    current_time = datetime.now().time()
+    start_date = obj.start_mailing
+    end_date = obj.end_mailing
+    time_sending = obj.time_sending
+
+    if (end_date > current_date > start_date or
+            end_date > current_date == start_date and time_sending < current_time or
+            end_date == current_date and time_sending > current_time or
+            start_date == current_date and time_sending > current_time):
+        obj.status = Mailing.STARTED
+    elif end_date == current_date and current_time > time_sending or current_date > end_date:
+        obj.status = Mailing.COMPLETED
+    obj.save()
+
+
+def change_date_next_sending(obj) -> None:
+    """Меняет дату следующей отправки в зависимости от выбранной пользователем периодичности"""
+    if obj.periodicity == Mailing.DAILY:
+        obj.next_sending += timedelta(days=1)
+    elif obj.periodicity == Mailing.WEEKLY:
+        obj.next_sending += timedelta(weeks=1)
+    elif obj.periodicity == Mailing.MONTHLY:
+        obj.next_sending += timedelta(days=30)
+    obj.save()
